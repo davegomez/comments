65f3ec4b16b62a7b63764e976e54991036bd00bf
David Gomez <dgomez@hugeinc.com>
2015-08-26 17:58:13 -0500

    Fetching from the server

---
+++ public/js/.module-cache/d697b01141157e597eaf2952d72342292756a176.js
@@ -0,0 +1,72 @@
+/* eslint
+   func-names: 0,
+   object-shorthand: 0,
+   react/react-in-jsx-scope: 0,
+   no-unused-vars: 0,
+   react/prop-types: 0,
+   react/no-danger: 0 */
+
+var PropTypes = React.PropTypes;
+var data = [
+  {author: 'Pete Hunt', text: 'This is one comment'},
+  {author: 'Jordan Walke', text: 'This is *another* comment'}
+];
+
+var CommentBox = React.createClass({displayName: "CommentBox",
+  render: function() {
+    console.log(data);
+    return (
+      React.createElement("div", {className: "commentBox"}, 
+        React.createElement("h1", null, "Comments"), 
+        React.createElement(CommentList, {data: this.props.data}), 
+        React.createElement(CommentForm, null)
+      )
+    );
+  }
+});
+
+var CommentList = React.createClass({displayName: "CommentList",
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        React.createElement(Comment, {author: comment.author}, 
+          comment.text
+        )
+      );
+    });
+    return (
+      React.createElement("div", {className: "commentList"}, 
+        commentNodes
+      )
+    );
+  }
+});
+
+var CommentForm = React.createClass({displayName: "CommentForm",
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentForm"}, 
+        "Hello, world! I am a CommentForm."
+      )
+    );
+  }
+});
+
+var Comment = React.createClass({displayName: "Comment",
+  render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return (
+      React.createElement("div", {className: "comment"}, 
+        React.createElement("h2", {className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.createElement("span", {dangerouslySetInnerHTML: {__html: rawMarkup}})
+      )
+    );
+  }
+});
+
+React.render(
+  React.createElement(CommentBox, {url: "comments.json"}),
+  document.getElementById('content')
+);
